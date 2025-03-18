9c64ee44ac35c963952ff47fe514863f0544c422
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2025-03-18 22:05:43 +0530

    Create J 10 (Constructor Overloading).java

---
+++ J 10 (Constructor Overloading).java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class test
+{
+	int a;
+	test()
+	{
+		a=0;
+	}
+
+	test(int x)
+	{
+		a=x;
+	}
+
+	public void display()
+	{
+		System.out.println(a+" ");
+	}
+
+	public static void main(String args[])
+	{
+		test ob1 = new test(10);
+		test ob2 = new test();
+
+		ob1.display();
+		ob2.display();
+		
+	}
+}
