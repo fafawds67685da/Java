e0a75953c1ef9cb92f91f2e346ef806eb507bcd8
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2025-03-18 21:24:18 +0530

    Create J 05 (Creating String using String buffer).java

---
+++ J 05 (Creating String using String buffer).java
@@ -0,0 +1,18 @@
+import java.util.*;
+class test
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String n;
+		System.out.println("Enter the String");
+		n = sc.nextLine();
+
+
+		StringBuffer sb = new StringBuffer(n);
+		System.out.println("String is "+sb);
+
+		  
+
+	}
+}
