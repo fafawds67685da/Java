5d1735f182e1465c39a75ab506e115eae74cbdfa
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2025-03-18 23:19:15 +0530

    Create J 15 (Creating package).java

---
+++ J 15 (Creating package).java
@@ -0,0 +1,14 @@
+package p;
+public class test
+{
+	public static void main(String args[])
+	{
+		System.out.println("My First Package");
+	}
+}
+
+
+/*
+
+command 1: javac -d . test.java
+command 2: java p.test
