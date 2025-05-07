525cb0b6c0035f319c2bfdb250f672f282b13fa2
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2025-05-07 10:31:23 +0530

    Update M 01 (Basic).java

--- Multi_Threading/M 01 (Basic).java
+++ Multi_Threading/M 01 (Basic).java
@@ -12,7 +12,11 @@ public class Sear extends Thread
 	public static void main(String args[])
 	{
 		Sear ob = new Sear();
-		ob.start();
+		ob.run(); //If you called ob.run() instead of ob.start(), 
+		//it would run like a normal method call on the main thread—not as a new thread.
+		
+		
+		//It calls ob.start(), which starts a new thread and calls the run() method in that new thread
 		for(int i =0;i<10;i++)
 		{
 			System.out.println("Main thread is empty");
